95ea03ebbd7b7162285e6971ef9728377afdca9c
Simon Hudon <simon.hudon@gmail.com>
2018-03-13 21:35:42 -0400

    Made some progress in `refineS` tactic

--- writing a tactic/refine.lean
+++ writing a tactic/refine.lean
@@ -1,4 +1,5 @@
 
+import data.dlist
 
 /- We want to write a set of tactics to automate the construction of
    instances. One approach might be to automate the specific instances
@@ -176,25 +177,152 @@ meta def mk_mvar_list : ℕ → tactic (list expr)
  | 0 := pure []
  | (succ n) := (::) <$> mk_mvar <*> mk_mvar_list n
 
-open interactive interactive.types
-#print structure_instance_info
-meta def tactic.interactive.refineS (e : parse texpr) : tactic unit :=
+open interactive interactive.types lean.parser
+     tactic.interactive (itactic)
+
+-- meta def tactic.interactive.refineS (e : parse texpr) : tactic unit :=
+-- do    str ← e.get_structure_instance_info,
+--       trace str.struct,
+--       trace str.sources,
+--       trace str.field_names,
+--       tgt ← target,
+--       let struct_n : name := tgt.get_app_fn.const_name,
+--       env ← get_env,
+--       exp_fields ← env.structure_fields struct_n,
+--       let missing_f := (exp_fields.diff str.field_names).diff [`to_has_map],
+--       vs ← mk_mvar_list missing_f.length,
+--       let e' : pexpr := pexpr.mk_structure_instance
+--           { -- struct := some struct_n
+--           -- ,
+--           field_names := str.field_names ++ missing_f
+--           , field_values := str.field_values ++ vs.map to_pexpr },
+--       refine ``(%%e' : %%tgt)
+
+meta def var_names : expr → list name
+ | (expr.pi n _ _ b) := n :: var_names b
+ | _ := []
+
+meta def drop_binders : expr → tactic expr
+ | (expr.pi n bi t b) := b.instantiate_var <$> mk_local' n bi t >>= drop_binders
+ | e := pure e
+
+meta def subobject_names (struct_n : name) : tactic (list name × list name) :=
+do env ← get_env,
+   [c] ← pure $ env.constructors_of struct_n | fail "too many constructors",
+   vs  ← var_names <$> (mk_const c >>= infer_type),
+   fields ← env.structure_fields struct_n,
+   return $ fields.partition (λ fn, ↑("_" ++ fn.to_string) ∈ vs)
+
+def dlist.join {α} : list (dlist α) → dlist α
+ | [] := dlist.empty
+ | (x :: xs) := x ++ dlist.join xs
+
+meta def expanded_field_list' : name → tactic (dlist $ name × name) | struct_n :=
+do (so,fs) ← subobject_names struct_n,
+   ts ← so.mmap (λ n, do
+     e ← mk_const (n.update_prefix struct_n) >>= infer_type >>= drop_binders,
+     expanded_field_list' $ e.get_app_fn.const_name),
+   -- trace so, trace fs,
+   return $ dlist.join ts ++ dlist.of_list (fs.map $ prod.mk struct_n)
+
+meta def expanded_field_list (struct_n : name) : tactic (list $ name × name) :=
+dlist.to_list <$> expanded_field_list' struct_n
+
+def mmap₂ {α β γ} {m : Type u → Type v} [applicative m] (f : α → β → m γ) : list α → list β → m (list γ)
+ | [] _ := pure []
+ | _ [] := pure []
+ | (x :: xs) (y :: ys) := (::) <$> f x y <*> mmap₂ xs ys
+
+def mmap₂' {α β γ} {m : Type u → Type v} [applicative m] (f : α → β → m γ) : list α → list β → m punit
+ | [] _ := pure punit.star
+ | _ [] := pure punit.star
+ | (x :: xs) (y :: ys) := f x y *> mmap₂' xs ys
+
+def sum.map_right {α β β'} (f : β → β') : α ⊕ β → α ⊕ β'
+ | (sum.inr x) := sum.inr $ f x
+ | (sum.inl x) := sum.inl x
+
+meta def tactic.interactive.refineS (e : parse texpr) (ph : parse $ optional $ tk "with" *> ident) : tactic unit :=
 do    str ← e.get_structure_instance_info,
-      trace str.struct,
-      trace str.sources,
-      trace str.field_names,
       tgt ← target,
       let struct_n : name := tgt.get_app_fn.const_name,
-      env ← get_env,
-      exp_fields ← env.structure_fields struct_n,
-      let missing_f := (exp_fields.diff str.field_names).diff [`to_has_map],
+      exp_fields ← expanded_field_list struct_n,
+      let exp_fields' := exp_fields.map prod.snd,
+      let missing_f := exp_fields.filter (λ f, (f.2 : name) ∉ str.field_names),
+      let provided := exp_fields.filter (λ f, (f.2 : name) ∈ str.field_names),
       vs ← mk_mvar_list missing_f.length,
       let e' : pexpr := pexpr.mk_structure_instance
-          { -- struct := some struct_n
-          -- ,
-          field_names := str.field_names ++ missing_f
+          { struct := some struct_n
+          , field_names := str.field_names ++ missing_f.map prod.snd
           , field_values := str.field_values ++ vs.map to_pexpr },
-      refine ``(%%e' : %%tgt)
+      refine e',
+      gs ← with_enable_tags (
+        mmap₂ (λ (n : name × name) v, do
+           set_goals [v],
+           try (interactive.unfold (provided.map $ λ ⟨s,f⟩, f.update_prefix s) (loc.ns [none])),
+           apply_auto_param
+             <|> apply_opt_param
+             <|> (do match ph with
+                       | (some ph) := () <$ (mk_const (n.2.update_prefix n.1) >>= pose ph none)
+                       | none := return ()
+                     end,
+                     set_main_tag [`_field,n.2]),
+           get_goals)
+        missing_f vs),
+      set_goals gs.join
+
+meta def collect_tagged_goals (pre : name) : tactic (list expr) :=
+do gs ← get_goals,
+   gs.mfoldr (λ g r, do
+      pre' :: t ← get_tag g,
+      if t = [pre] ∧ pre' = pre'.get_prefix <.> "_field"
+         then return (g::r)
+         else return r)
+      []
+
+-- meta def match_field_tag
+
+meta def tactic.interactive.field (tag : parse ident) (tac : itactic) : tactic unit :=
+do ts ← collect_tagged_goals tag,
+   match ts with
+    | [] := fail format!"no field goal with tag {tag}"
+    | [g] := do
+      gs ← get_goals,
+      set_goals $ g :: gs.filter (≠ g),
+      solve1 tac
+    | _ := fail format!"multiple goals have tag {tag}"
+   end
+universe w
+
+instance {α : Type*} : functor (sum α) :=
+  -- (we use `.{v}` above so that Lean won't have to guess the universes which would
+  -- create visual noise
+begin
+  refineS { map := @sum.map_right _, .. } with law,
+  all_goals { intros, casesm _ ⊕ _ ; refl },
+end
+
+structure except_t (f : Type u → Type v) (α : Type u) :=
+ (run : f (string ⊕ α))
+
+open has_map functor
+def except_t.map {F} [functor F] {α β} (f : α → β) : except_t F α → except_t F β
+ | ⟨ x ⟩ := ⟨ @has_map.map _ sum.functor.to_has_map _ _ f <$> x ⟩
+
+lemma map_comp' {f : Type u → Type v} [functor f] {α β γ : Type u} (g : β → γ) (h : α → β) :
+  map g ∘ map h = (map (g ∘ h) : f α → f γ) :=
+by funext x ; rw [map_comp]
+
+lemma map_id' {f : Type u → Type v} [functor f] {α} :
+  (map id : f α → f α) = id :=
+by funext x ; rw [id_map,id]
+
+example {α : Type u} {f : Type u → Type w} [functor f] : functor (except_t f) :=
+begin
+  refineS { map := @except_t.map _ _ } with law
+  ; intros ; casesm except_t f _ ; simp [except_t.map,map_id'],
+  rw [← law,map_comp'],
+end
 
 example {α : Type u} : functor.{v} (sum α) :=
   -- (we use `.{v}` above so that Lean won't have to guess the universes which would
@@ -211,20 +339,29 @@ begin
     -- functor
     -- (some [to_has_map, map_const_eq, id_map, map_comp])
   -- refine { map := sorry, .. },
-  refineS { map := sorry },
-  (do let e : pexpr := ``({ map := sum.map_right }),
-      str ← e.get_structure_instance_info,
-      tgt ← target,
-      let struct_n : name := tgt.get_app_fn.const_name,
-      env ← get_env,
-      exp_fields ← env.structure_fields struct_n,
-      let missing_f := exp_fields.diff str.field_names,
-      vs ← mk_mvar_list missing_f.length,
-      let e' : pexpr := pexpr.mk_structure_instance
-          { -- struct := some struct_n
-          -- ,
-          field_names := str.field_names ++ missing_f
-          , field_values := str.field_values ++ vs.map to_pexpr },
-      refine e'),
-  admit
+  -- refineS { map := sorry },
+  -- (do fs ← expanded_field_list `applicative,
+  --     trace fs ),
+  refineS { map := @sum.map_right _, .. } with field,
+  -- field map_const_eq { dunfold has_map.map, tactic.apply_auto_param },
+  field map_comp { intros, casesm _ ⊕ _ ; refl },
+  field id_map { intros, casesm _ ⊕ _ ; refl },
+  -- trace_result
+  -- (do let e : pexpr := ``({ map := @sum.map_right _ }),
+  --     str ← e.get_structure_instance_info,
+  --     tgt ← target,
+  --     let struct_n : name := tgt.get_app_fn.const_name,
+  --     exp_fields ← expanded_field_list struct_n,
+  --     let missing_f := exp_fields.diff str.field_names,
+  --     vs ← mk_mvar_list missing_f.length,
+  --     trace str.field_values,
+  --     trace str.field_names,
+  --     trace missing_f,
+  --     let e' : pexpr := pexpr.mk_structure_instance
+  --         { struct := some struct_n
+  --         , field_names := str.field_names ++ missing_f
+  --         , field_values := str.field_values ++ vs.map to_pexpr },
+  --     trace e',
+  --     refine e'),
+  -- admit,
 end
